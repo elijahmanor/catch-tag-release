c7ca48848af4347b6d92f78c9f881b0b40de3a79
Robert Reaves <robert.reaves345@gmail.com>
2018-06-19 12:48:01 -0500

    Added start script

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 const tagRelease = require( "tag-release" );
-console.log( "Hello World!!" );
-console.log( "wuz up?" );
-console.log( 'howdy' );
-console.log( 'howdy' );
-console.log( 'howdy' );
-console.log( 'howdy' );
-console.log( 'howdy' );
 
 tagRelease.run({release:'minor'}).then(result=>{
 	console.log( "We did it:", result);
