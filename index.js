09227193b4eb6343cd22709c118ceaae39432490
Elijah Manor <elijah.manor@gmail.com>
2018-06-26 13:32:09 -0500

    conflict

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const tagRelease = require( "tag-release" );
 
-tagRelease.run({release:'patch', cwd: "." }).then(result=>{
-	console.log( "We did it:", result);
-}).catch(error=>{
-	console.log("BOOM:", error );
-});
+tagRelease.run( {
+	release: "patch",
+	cwd: "./"
+} ).then( result => {
+	console.log( "We did it:", result );
+} ).catch( error => {
+	console.log( "BOOM:", error );
+} );
+
