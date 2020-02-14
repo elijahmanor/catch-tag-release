206ef122e84ce185bc041cba747fac844ca1fe4b
Robert Reaves <robert.reaves345@gmail.com>
2020-02-14 06:27:31 -0600

    Added sleeps to scripts

--- postStart.js
+++ postStart.js
@@ -1,1 +1,8 @@
-console.log( "Hello from postStart.js" );
+const script = async () => {
+	await new Promise(r => setTimeout(r, 3000));
+	console.log( "Hello from post script" );
+}
+
+console.log( "post script before" );
+script();
+console.log( "post script done" );
--- preStart.js
+++ preStart.js
@@ -1,1 +1,8 @@
-console.log( "Hello from preStart.js" );
+const script = async () => {
+	await new Promise(r => setTimeout(r, 3000));
+	console.log( "Hello from pre script" );
+}
+
+console.log( "post script before" );
+script();
+console.log( "pre script done" );
